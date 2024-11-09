a08d16430e9b6865b599e8abf6b3020c87e38caa
Skibidicatlol <lfilgueirasdasilva08@gmail.com>
2024-11-09 16:23:37 -0300

    Update Five Seasons.lua

--- Five Seasons.lua
+++ Five Seasons.lua
@@ -315,7 +315,7 @@ spawn(function()
     local GUI = FiveEffects.FXUi:Clone()
     GUI.Parent = localPlayer.PlayerGui
 
-    wait(12)
+    wait(14.5)
     local fiveSeasonsDots = GUI:FindFirstChild("FiveSeasonsDots")
     if fiveSeasonsDots then
         print("FiveSeasonsDots found and animating")
@@ -342,7 +342,7 @@ end)
 
 -- Second spawn function for handling the visibility of other UI elements
 spawn(function()
-    wait(12)
+    wait(14.5)
         -- Clone FXUi into PlayerGui
         local GUI = game.ReplicatedStorage.Resources.FiveSeasonsFX.FXUi:Clone()
         GUI.Parent = game.Players.LocalPlayer.PlayerGui
