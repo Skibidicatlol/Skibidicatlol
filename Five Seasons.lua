9ac705c79af5b8ce1b74381a2189e8bce33ce8a1
Skibidicatlol <lfilgueirasdasilva08@gmail.com>
2024-11-09 16:32:10 -0300

    Update Five Seasons.lua

--- Five Seasons.lua
+++ Five Seasons.lua
@@ -526,7 +526,7 @@ end)
             
         end)
         spawn(function()
-            wait(17.75)
+            wait(18.75)
             local fxUi = localPlayer:WaitForChild("PlayerGui"):WaitForChild("FXUi")
             local impactFrames = fxUi:WaitForChild("ImpactFrames")
             local impactsound = Instance.new("Sound", character.Torso)
@@ -610,7 +610,7 @@ local function createFistModel()
 
     -- Animate the fist model
     for i = 1, 5 do
-        wait(0.375)
+        wait(0.25)
         fistModel:SetPrimaryPartCFrame(fistModel.PrimaryPart.CFrame + Vector3.new(0, -6.2, 0))
     end
     for i = 1, 5 do        
