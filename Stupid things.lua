7f1c024944ce57a78f04b84fc8894f06dd41cc01
Skibidicatlol <lfilgueirasdasilva08@gmail.com>
2024-11-09 16:09:50 -0300

    Create Stupid things.lua

---
+++ Stupid things.lua
@@ -0,0 +1,157 @@
+-- Pinpoint thing blade master
+local vfx = game.ReplicatedStorage.Resources.Pinpoint["Ground"].Ground.Ground:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.PinpointFinisher["Slash"].Slash.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Pinpoint["Dash"].Dash.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+-- sunrise
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FinalFX"].Explode.C:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FinalFX"].Explode.B:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FinalFX"].Explode.A:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FinalFX"].Explode.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["Forward"].Part.Flarey:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["Forward"].Part.Windy:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["Forward"].Part.Flamey:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FloorAdjusted"].Floor.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["Final"].Final.Another:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["FireStuff"].Stomp.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.Sunrise["AtomicSlash2"].Part.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+-- Split Counter
+
+local vfx = game.ReplicatedStorage.Resources.SplitCounter["Bruh"].Bruh.Bruh:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.SplitCounter["TPNEW"].TP.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+-- Crab
+
+local vfx = game.ReplicatedStorage.Resources.CrabBeam["Blaswwwt"].dashpunch.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.CrabBeam["Beamy"].End.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
+
+local vfx = game.ReplicatedStorage.Resources.SplitCounter["Slash"].Slash.Attachment:Clone()
+vfx.Parent = game.Players.LocalPlayer.Character["HumanoidRootPart"]
+    for _, child in ipairs(vfx:GetChildren()) do
+        if child:IsA("ParticleEmitter") then
+child:Emit(10)
+        end
+end
